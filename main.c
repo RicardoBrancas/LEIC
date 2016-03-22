9829c09a3164a5f2c3e5e354654a83e713a9b486
ricardo <RicardoBrancas>
2016-03-22 15:44:01 +0000

    Base code

--- main.c
+++ main.c
@@ -1,3 +1,34 @@
-int main() {
+#include <stdio.h>
+
+#define MAXAEROPORTO 1000
+#define NOMEAEROPORTO 3
+
+typedef struct {
+  char nome[NOMEAEROPORTO+1];
+  int capacidade;
+  short estado;
+} Aeroporto;
+
+void func_A();
+
+int main(){
+  Aeroporto aeroportos[MAXAEROPORTO];
+  int voos[MAXAEROPORTO][MAXAEROPORTO];
+  int posAeroporto;
+
+  int option;
+  while((option = getchar()) != 'X') {
+    switch(option) {
+      case 'A':
+        func_A();
+        break;
+    }
+  }
+
+
   return 0;
+}
+
+void func_A() {
+  scanf("%s %d");
 }
