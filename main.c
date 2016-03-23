880f5bee17c2f3492084d9fa3809be0ade6f5ff3
ricardo <RicardoBrancas>
2016-03-23 23:22:29 +0000

    Added functions R, N, Q, C and L (case 1)

--- main.c
+++ main.c
@@ -14,6 +14,13 @@ void func_A(Aeroporto[], int);
 
 void func_F(Aeroporto[], int[][MAXAEROPORTO], int);
 
+void func_R(Aeroporto[], int[][MAXAEROPORTO], int);
+
+void func_N(Aeroporto[], int[][MAXAEROPORTO], int);
+
+void func_Q(Aeroporto[], int[][MAXAEROPORTO], int);
+
+void func_C(Aeroporto[], int[][MAXAEROPORTO], int);
 
 void func_L(Aeroporto[], int[][MAXAEROPORTO], int);
 
@@ -31,6 +38,18 @@ int main() {
 						break;
 				case 'F':
 						func_F(aeroportos, voos, posAeroporto);
+						break;
+				case 'R':
+						func_R(aeroportos, voos, posAeroporto);
+						break;
+				case 'N':
+						func_N(aeroportos, voos, posAeroporto);
+						break;
+				case 'Q':
+						func_Q(aeroportos, voos, posAeroporto);
+						break;
+				case 'C':
+						func_C(aeroportos, voos, posAeroporto);
 						break;
 				case 'L':
 						func_L(aeroportos, voos, posAeroporto);
@@ -77,6 +96,15 @@ int voosTotais(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int aeroporto, 
 		return voosTotais;
 }
 
+int conexoes(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int aeroporto, int posMax) {
+		int i = 0, conexoes = 0;
+		for(i = 0; i < posMax; i++) {
+				if (voos[aeroporto][i] > 0)
+						conexoes++;
+		}
+		return conexoes;
+}
+
 void func_A(Aeroporto aeroportos[], int pos) {
 		Aeroporto aero;
 		scanf("%s %d", aero.nome, &aero.capacidade);
@@ -96,6 +124,64 @@ void func_F(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int posMax) {
 				voos[pos2][pos1]++;
 		} else {
 				printf("*Impossivel adicionar voo %s %s\n", nome1, nome2);
+		}
+}
+
+void func_R(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int posMax) {
+		char nome1[NOMEAEROPORTO + 1], nome2[NOMEAEROPORTO + 1];
+		scanf("%s %s", nome1, nome2);
+		int pos1 = procuraAeroporto(aeroportos, posMax, nome1);
+		int pos2 = procuraAeroporto(aeroportos, posMax, nome2);
+		if (pos1 >= 0 && pos2 >= 0 && voos[pos1][pos2] > 0) {
+				voos[pos1][pos2]--;
+		} else {
+				printf("*Impossivel remover voo %s %s\n", nome1, nome2);
+		}
+}
+
+void func_N(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int posMax) {
+		char nome1[NOMEAEROPORTO + 1], nome2[NOMEAEROPORTO + 1];
+		scanf("%s %s", nome1, nome2);
+		int pos1 = procuraAeroporto(aeroportos, posMax, nome1);
+		int pos2 = procuraAeroporto(aeroportos, posMax, nome2);
+		if (pos1 >= 0 && pos2 >= 0) {
+				printf("Voos entre cidades %s:%s:%d:%d\n",
+				       nome1, nome2, voos[pos1][pos2], voos[pos2][pos1]);
+		} else {
+				if (pos1 < 0) {
+						printf("*Aeroporto %s inexistente\n", nome1);
+				}
+				if (pos2 < 0) {
+						printf("*Aeroporto %s inexistente\n", nome2);
+				}
+		}
+}
+
+void func_Q(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int posMax) {
+		int i = 0, aeroporto = 0, conex = 0;
+		for (i = 0; i < posMax; i++) {
+				int con = conexoes(aeroportos, voos, i, posMax);
+				if(con > conex) {
+						aeroporto = i;
+						conex = con;
+				}
+		}
+		printf("Aeroporto com mais ligacoes %s:%d\n", aeroportos[aeroporto].nome, conex);
+}
+
+void func_C(Aeroporto aeroportos[], int voos[][MAXAEROPORTO], int posMax) {
+		char nome[NOMEAEROPORTO + 1];
+		scanf("%s", nome);
+		int pos = procuraAeroporto(aeroportos, posMax, nome);
+		if(pos >= 0) {
+        aeroportos[pos].estado = 0;
+        int i;
+        for(i = 0; i < posMax; i++) {
+          voos[pos][i] = 0;
+          voos[i][pos] = 0;
+        }
+		} else {
+				printf("*Aeroporto %s inexistente\n", nome);
 		}
 }
 
