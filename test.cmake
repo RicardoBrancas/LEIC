219676b0c9f9f56ed9865cb803da2e90aec1e5b2
Ricardo Brancas <anjomigel@gmail.com>
2016-05-01 13:09:15 +0100

    Initial commit

---
+++ test.cmake
@@ -0,0 +1,14 @@
+execute_process(
+ COMMAND ${test_cmd}
+ INPUT_FILE ${input_test}
+ OUTPUT_FILE ${output_test}
+ TIMEOUT 10
+)
+execute_process(
+  COMMAND diff ${output_blessed} ${output_test}
+  RESULT_VARIABLE test_not_successful
+)
+
+if( test_not_successful )
+ message( SEND_ERROR "" )
+endif( test_not_successful )
