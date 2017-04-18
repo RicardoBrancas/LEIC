510e479cf9e7dfeaafb26d92083f233e02fbb2d5
David Nunes <David@David-OpenSUSE.home>
2017-04-18 10:54:21 +0100

    2 options for each menu completed

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -120,7 +120,7 @@ var menu = [
   {
     name: "Salada de Salmão Fumado",
     id: 7,
-    price: 7.5,
+    price: 8.5,
     categories: [
         "saladas"
     ],
@@ -171,6 +171,134 @@ var menu = [
       "Sódio": "77 mg"
     }
   },
+  {
+    name: "Salada de Frango",
+    id: 10,
+    price: 7.5,
+    categories: [
+        "saladas"
+    ],
+    nutrition: {
+      "Energia": "386 kCal",
+      "Lípidos": "18.5 g",
+      "\tdos quais saturados": "5.7 g",
+      "Colesterol": "181 mg",
+      "Hidratos de Carbono": "18.9 g",
+      "\tdos quais açucares": "1.5 g",
+      "Proteína": "24.3 g",
+      "Sódio": "336 mg"
+    }
+  },
+  {
+    name: "Esparguete à Carbonara",
+    id: 11,
+    price: 9,
+    categories: [
+        "destaques",
+        "pratos-principais-carne"
+    ],
+    nutrition: {
+      "Energia": "646 kCal",
+      "Lípidos": "27.2 g",
+      "\tdos quais saturados": "6.7 g",
+      "Colesterol": "234 mg",
+      "Hidratos de Carbono": "38.9 g",
+      "\tdos quais açucares": "6.5 g",
+      "Proteína": "29.3 g",
+      "Sódio": "476 mg"
+    }
+  },
+  {
+    name: "Bife de Atum de Cebolada com Batatas",
+    id: 12,
+    price: 8.5,
+    categories: [
+        "pratos-principais-peixe"
+    ],
+    nutrition: {
+      "Energia": "624 kCal",
+      "Lípidos": "21.4 g",
+      "\tdos quais saturados": "6.1 g",
+      "Colesterol": "279 mg",
+      "Hidratos de Carbono": "41.7 g",
+      "\tdos quais açucares": "7.5 g",
+      "Proteína": "32.3 g",
+      "Sódio": "376 mg"
+    }
+  },
+  {
+    name: "Risoto de Cenoura",
+    id: 13,
+    price: 6.5,
+    categories: [
+        "pratos-principais-vegetariano"
+    ],
+    nutrition: {
+      "Energia": "397 kCal",
+      "Lípidos": "16.1 g",
+      "\tdos quais saturados": "3.4 g",
+      "Colesterol": "173 mg",
+      "Hidratos de Carbono": "45.9 g",
+      "\tdos quais açucares": "7.8 g",
+      "Proteína": "13.7 g",
+      "Sódio": "217 mg"
+    }
+  },
+  {
+    name: "Ratatouille",
+    id: 14,
+    price: 10.5,
+    categories: [
+      "destaques",
+        "pratos-principais-vegetariano"
+    ],
+    nutrition: {
+      "Energia": "478 kCal",
+      "Lípidos": "23.7 g",
+      "\tdos quais saturados": "8.4 g",
+      "Colesterol": "239 mg",
+      "Hidratos de Carbono": "41.3 g",
+      "\tdos quais açucares": "6.9 g",
+      "Proteína": "14.6 g",
+      "Sódio": "239 mg"
+    }
+  },
+  {
+    name: "Bolo de Bolacha",
+    id: 15,
+    price: 2.5,
+    categories: [
+        "sobremesas"
+    ],
+    nutrition: {
+      "Energia": "282 kCal",
+      "Lípidos": "20.1 g",
+      "\tdos quais saturados": "9.2 g",
+      "Colesterol": "267 mg",
+      "Hidratos de Carbono": "26.9 g",
+      "\tdos quais açucares": "18.8 g",
+      "Proteína": "4.7 g",
+      "Sódio": "12 mg"
+    }
+  },
+  {
+  name: "Mousse de Chocolate",
+  id: 16,
+  price: 2.5,
+  categories: [
+      "sobremesas"
+  ],
+  nutrition: {
+    "Energia": "296 kCal",
+    "Lípidos": "22.7 g",
+    "\tdos quais saturados": "11.3 g",
+    "Colesterol": "246 mg",
+    "Hidratos de Carbono": "24.6 g",
+    "\tdos quais açucares": "19.3 g",
+    "Proteína": "3.8 g",
+    "Sódio": "8 mg"
+  }
+},
 ]
 
 var categories = [];
