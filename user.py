ded894cf7ecf1a6a14ea95a728dba73c8c7e930f
Ricardo Brancas <anjomigel@gmail.com>
2017-09-26 16:36:49 +0100

    c2

--- user.py
+++ user.py
@@ -8,9 +8,7 @@ parser.add_argument('-p', default=58016)
 args = parser.parse_args()
 
 host = args.n
-port = args.p
-
-hostaddr = socket.gethostbyname(host);
+port = int(args.p)
 
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,7 +16,17 @@ except Exception:
     print("Error creating socket")
     exit()
 
-print("Trying to connect to host " + str(hostaddr) + " on port " + str(port))
-sock.connect((hostaddr, port))
+print("Trying to connect to host " + host + " on port " + str(port))
+sock.connect((host, port))
+
+while True:
+  line = sys.stdin.readline()
+  lineLst = line.split(' ')
+  
+  if lineLst[0] == 'list':
+    print("Asking for list")
+    sock.sendall(b'LST\n')
+    result = sock.recv(1024)
+    print(result)
 
 sock.close()
