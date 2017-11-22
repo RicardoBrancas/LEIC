27c7d395a42cc582ddff85f418805d56e71d102d
Ricardo Brancas <anjomigel@gmail.com>
2017-11-22 15:24:20 +0000

    Fixed identation

--- P3/RLsol.py
+++ P3/RLsol.py
@@ -6,23 +6,19 @@ Created on Mon Oct 16 20:31:54 2017
 """
 import numpy as np
 
-def Q2pol(Q, eta=5):
-	return 
-	
-class myRL:
 
+def Q2pol(Q, eta=5):
+    return
+
+
+class myRL:
     def __init__(self, nS, nA, gamma):
         self.nS = nS
         self.nA = nA
         self.gamma = gamma
-        self.Q = np.zeros((nS,nA))
-        
+        self.Q = np.zeros((nS, nA))
+
     def traces2Q(self, trace):
         # implementar esta funcao
-        
+
         return self.Q
-
-
-
-
-            
