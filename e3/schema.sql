af419c23245dc72b80d9af4b3798f2618607618d
mihail1996 <mihail.brinza96@hotmail.com>
2017-11-23 15:37:23 +0000

    fixed some bugs

--- e3/schema.sql
+++ e3/schema.sql
@@ -1,102 +1,107 @@
-create table categoria(
-  nome varchar(80) not null,
-  primary key(nome),
-  check(nome in (select nome   --RE1, RE2
-                from (select nome from categoria_simples UNION
-                      select nome from super_categoria)
-                having count(nome) = 1))
+CREATE TABLE categoria (
+  nome VARCHAR(80) NOT NULL,
+  PRIMARY KEY (nome),
+  CHECK (nome IN (SELECT nome --RE1, RE2
+                  FROM (SELECT nome
+                         FROM categoria_simples
+                        UNION
+                        SELECT nome
+                         FROM super_categoria) AS x
+                  HAVING count(nome) = 1))
 );
 
-create table categoria_simples(
-  nome varchar(80) not null,
-  primary key(nome),
-  foreign key(nome)
-    references categoria(nome)
+CREATE TABLE categoria_simples (
+  nome VARCHAR(80) NOT NULL,
+  PRIMARY KEY (nome),
+  FOREIGN KEY (nome) REFERENCES categoria (nome)
 );
 
-create table super_categoria(
-  nome varchar(80) not null,
-  primary key(nome),
-  foreign key(nome) references categoria(nome),
-  check (nome in (select super_categoria from constituida)) --sor ricardo, has de ver isto
+CREATE TABLE super_categoria (
+  nome VARCHAR(80) NOT NULL,
+  PRIMARY KEY (nome),
+  FOREIGN KEY (nome) REFERENCES categoria (nome),
+  CHECK (nome IN (SELECT super_categoria
+                  FROM constituida)) --sor ricardo, has de ver isto
 );
 
-create table constituida(
-  super_categoria varchar(80) not null,
-  categoria varchar(80) not null,
-  primary key(super_categoria, categoria),
-  foreign key(super_categoria) references super_categoria(nome),
-  foreign key(categoria) references categoria(nome),
-  check(super_categoria <> categoria)
+CREATE TABLE constituida (
+  super_categoria VARCHAR(80) NOT NULL,
+  categoria       VARCHAR(80) NOT NULL,
+  PRIMARY KEY (super_categoria, categoria),
+  FOREIGN KEY (super_categoria) REFERENCES super_categoria (nome),
+  FOREIGN KEY (categoria) REFERENCES categoria (nome),
+  CHECK (super_categoria <> categoria)
 );
 
-create table produto(
-  ean numeric(13) not null,
-  design varchar(80) not null,
-  categoria varchar(80) not null,
-  forn_primario varchar(80) not null,
-  primary key(ean),
-  foreign key(categoria) references categoria(nome),
-  foreign key(forn_primario) references fornecedor(nif)
+CREATE TABLE produto (
+  ean           NUMERIC(13) NOT NULL,
+  design        VARCHAR(80) NOT NULL,
+  categoria     VARCHAR(80) NOT NULL,
+  forn_primario VARCHAR(80) NOT NULL,
+  PRIMARY KEY (ean),
+  FOREIGN KEY (categoria) REFERENCES categoria (nome),
+  FOREIGN KEY (forn_primario) REFERENCES fornecedor (nif)
 );
 
-create table fornecedor(
-  nif numeric(9) not null,
-  nome varchar(80) not null,
-  primary key(nif)
+CREATE TABLE fornecedor (
+  nif  NUMERIC(9)  NOT NULL,
+  nome VARCHAR(80) NOT NULL,
+  PRIMARY KEY (nif)
 );
 
-create table fornece_sec(
-  nif numeric(9) not null,
-  ean numeric(13) not null,
-  foreign key(nif) references fornecedor(nif),
-  foreign key(ean) references produto(ean)
+CREATE TABLE fornece_sec (
+  nif NUMERIC(9)  NOT NULL,
+  ean NUMERIC(13) NOT NULL,
+  FOREIGN KEY (nif) REFERENCES fornecedor (nif),
+  FOREIGN KEY (ean) REFERENCES produto (ean)
 );
 
-create table corredor(
-  nro varchar(4) not null,
-  largura varchar(3) not null,
-  primary key(nro)
+CREATE TABLE corredor (
+  nro     VARCHAR(4) NOT NULL,
+  largura VARCHAR(3) NOT NULL,
+  PRIMARY KEY (nro)
 );
 
-create table prateleira(
-  nro varchar(4) not null,
-  lado varchar(10) not null,
-  altura varchar(10) not null,
-  primary key(nro, lado, altura),
-  foreign key(nro) references corredor(nro),
-  check (lado in ('esquerdo', 'direito')),
-  check (altura in ('chao', 'medio', 'superior'))
+CREATE TABLE prateleira (
+  nro    VARCHAR(4)  NOT NULL,
+  lado   VARCHAR(10) NOT NULL,
+  altura VARCHAR(10) NOT NULL,
+  PRIMARY KEY (nro, lado, altura),
+  FOREIGN KEY (nro) REFERENCES corredor (nro),
+  CHECK (lado IN ('esquerdo', 'direito')),
+  CHECK (altura IN ('chao', 'medio', 'superior'))
 );
 
-create table planograma(
-  ean numeric(13) not null,
-  nro varchar(4) not null,
-  lado varchar(10) not null,
-  altura varchar(10) not null,
-  face varchar(5) not null,
-  unidades varchar(4) not null,
-  primary key(ean, nro, lado, altura),
-  foreign key(ean) references produto(ean),
-  foreign key(nro, lado, altura) references prateleira(nro, lado, altura)
+CREATE TABLE planograma (
+  ean      NUMERIC(13) NOT NULL,
+  nro      VARCHAR(4)  NOT NULL,
+  lado     VARCHAR(10) NOT NULL,
+  altura   VARCHAR(10) NOT NULL,
+  face     VARCHAR(5)  NOT NULL,
+  unidades VARCHAR(4)  NOT NULL,
+  PRIMARY KEY (ean, nro, lado, altura),
+  FOREIGN KEY (ean) REFERENCES produto (ean),
+  FOREIGN KEY (nro, lado, altura) REFERENCES prateleira (nro, lado, altura)
 );
 
-create table evento_reposição(
-  operador varchar(80) not null,
-  instante timestamp not null,
-  primary key(operador),
-  primary key(instante),
-  check(instante > CURRENT_TIMESTAMP)
+CREATE TABLE evento_reposicao (
+  operador VARCHAR(80) NOT NULL,
+  instante TIMESTAMP   NOT NULL,
+  PRIMARY KEY (operador),
+  UNIQUE (instante),
+  CHECK (instante > CURRENT_TIMESTAMP)
 );
 
-create table reposicao(
-  ean numeric(13) not null,
-  nro varchar(4) not null,
-  lado varchar(10) not null,
-  altura varchar(10) not null,
-  unidades varchar(80) not null,
-  primary key(ean, nro, lado, altura),
-  primary key(operador),
-  primary key(instante),
-  foreign key(ean, nro, lado, altura) references planograma(ean, nro, lado, altura),
+CREATE TABLE reposicao (
+  ean      NUMERIC(13) NOT NULL,
+  nro      VARCHAR(4)  NOT NULL,
+  lado     VARCHAR(10) NOT NULL,
+  altura   VARCHAR(10) NOT NULL,
+  unidades VARCHAR(80) NOT NULL,
+  operador VARCHAR(80) NOT NULL,
+  instante TIMESTAMP   NOT NULL,
+  PRIMARY KEY (ean, nro, lado, altura),
+  UNIQUE (operador),
+  UNIQUE (instante),
+  FOREIGN KEY (ean, nro, lado, altura) REFERENCES planograma (ean, nro, lado, altura)
 );
