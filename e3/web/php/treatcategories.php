d3fd2cbca75db8150e1e7289a82cb837a9d6657f
mihail1996 <mihail.brinza96@hotmail.com>
2017-11-25 20:40:16 +0000

    first 2 done

--- e3/web/php/treatcategories.php
+++ e3/web/php/treatcategories.php
@@ -20,15 +20,15 @@ try {
     }
 
     if ($catremove != "") {
-        $sql = "DELETE FROM categoria WHERE nome=?;";
-        $stmt = $db->prepare($sql);
+        $stmt = $db->prepare("DELETE FROM categoria WHERE nome=?;");
+        $db->beginTransaction();
         $stmt->execute([$catremove]);
         $db->commit();
     }
 
     if ($subcatinsert != "" and $supercat1 != "") {
-        $sql = "INSERT INTO constituida VALUES (:super, :sub);";
-        $stmt = $db->prepare($sql);
+        $stmt = $db->prepare("INSERT INTO constituida VALUES (:super, :sub);");
+        $db->beginTransaction();
         $stmt->execute(['super' => $supercat1, 'sub' => $subcatinsert]);
         $db->commit();
     }
@@ -37,6 +37,7 @@ try {
         $stmt1 = $db->prepare('DELETE FROM constituida WHERE super_categoria AND categoria = (:super, :sub);');
         $stmt2 = $db->prepare('DELETE FROM categoria_simples WHERE categoria=?');
         $stmt3 = $db->prepare('DELETE FROM categoria WHERE nome=?');
+        $db->beginTransaction();
         $stmt1->execute(['super' => $supercat2, 'sub' => $subcatremove]);
         $stmt2->execute([$subcatremove]);
         $stmt3->execute([$subcatremove]);
