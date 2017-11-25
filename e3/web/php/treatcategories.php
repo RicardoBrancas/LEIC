b3e33ff6babd3f0cd3c8bb01575d9c674fbe36fb
mihail1996 <mihail.brinza96@hotmail.com>
2017-11-25 20:04:11 +0000

    maybe working

--- e3/web/php/treatcategories.php
+++ e3/web/php/treatcategories.php
@@ -1,39 +1,51 @@
 <?php
 try {
-	require 'connection.php';
+    require 'connection.php';
 
-	$catinsert = $_POST["categoryins"];
-	$catremove = $_POST["categoryremove"];
-	$subcatinsert = $_POST["categoryins1"];
-	$supercat1 = $_POST["categoryins2"];
-	$subcatremove = $_POST["subcategoryremove1"];
-	$supercat2 = $_POST["subcategoryremove2"];
+    $catinsert = $_POST["categoryins"];
+    $catremove = $_POST["categoryremove"];
+    $subcatinsert = $_POST["categoryins1"];
+    $supercat1 = $_POST["categoryins2"];
+    $subcatremove = $_POST["subcategoryremove1"];
+    $supercat2 = $_POST["subcategoryremove2"];
 
-	if ($catinsert != "") {
-		$stmt1 = $db->prepare('INSERT INTO categoria VALUES (?);');
-		$stmt2 = $db->prepare('INSERT INTO categoria_simples VALUES(?);');
+    if ($catinsert != "") {
+        $stmt1 = $db->prepare('INSERT INTO categoria VALUES (?);');
+        $stmt2 = $db->prepare('INSERT INTO categoria_simples VALUES(?);');
 
-		$db->beginTransaction();
-		$stmt1->execute([$catinsert]);
-		$stmt2->execute([$catinsert]);
-		$db->commit();
-	}
+        $db->beginTransaction();
+        $stmt1->execute([$catinsert]);
+        $stmt2->execute([$catinsert]);
+        $db->commit();
+    }
 
-	if ($catremove != "") {
-		$sql = "DELETE FROM categoria WHERE nome=?;";
-		$stmt = $db->prepare($sql);
-		$stmt->execute([$catremove]);
-	}
+    if ($catremove != "") {
+        $sql = "DELETE FROM categoria WHERE nome=?;";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$catremove]);
+        $db->commit();
+    }
 
-	if ($subcatinsert != "" and $supercat1 != "") {
-		$sql = "INSERT INTO constituida VALUES (:super, :sub);";
-		$stmt = $db->prepare($sql);
-		$stmt->execute(['super' => $supercat1, 'sub' => $subcatinsert]);
-	}
+    if ($subcatinsert != "" and $supercat1 != "") {
+        $sql = "INSERT INTO constituida VALUES (:super, :sub);";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(['super' => $supercat1, 'sub' => $subcatinsert]);
+        $db->commit();
+    }
+
+    if ($subcatremove != "" and $supercat2 != "") {
+        $stmt1 = $db->prepare('DELETE FROM constituida WHERE super_categoria AND categoria = (:super, :sub);');
+        $stmt2 = $db->prepare('DELETE FROM categoria_simples WHERE categoria=?');
+        $stmt3 = $db->prepare('DELETE FROM categoria WHERE nome=?');
+        $stmt1->execute(['super' => $supercat2, 'sub' => $subcatremove]);
+        $stmt2->execute([$subcatremove]);
+        $stmt3->execute([$subcatremove]);
+        $db->commit();
+    }
 
 
-	$db = null;
+    $db = null;
 } catch (PDOException $e) {
-	echo("<p>ERROR: {$e->getMessage()}</p>");
+    echo("<p>ERROR: {$e->getMessage()}</p>");
 }
 ?>
