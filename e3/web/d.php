53b48bd645610eaf5c7a5e4012929f7f472c72db
davidnunes25 <davidnunes25@github.com>
2017-11-27 00:27:08 +0000

    testing c

--- e3/web/d.php
+++ e3/web/d.php
@@ -1,3 +1,11 @@
+<?php
+require 'php/connection.php';
+
+$sql = 'SELECT (ean,nome) FROM produto';
+
+$result = $db->query($sql);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,9 +13,15 @@
 </head>
 <body>
 <h3>Alterar designacao do produto</h3>
-<form action="/treatdesign.php" method="POST">
+<form action="php/treatdesign.php" method="POST">
 	Insira o EAN do produto: <br>
-	<input type="text" name="ean"> <br>
+	<select name="ean">
+		<?php
+            while ($row = $result->fetch()) {
+		echo "<option>$row[0]</option>\n";
+		}
+		?>
+	</select>
 	Insira a nova designacao para esse produto: <br>
 	<input type="text" name="novadesign"> <br>
 	<input type="submit" value="Submit">
--- e3/web/php/treatdesign.php
+++ e3/web/php/treatdesign.php
@@ -8,10 +8,8 @@ try {
 	if (is_numeric($ean) and strlen($ean) == 13)
 		if ($newdesign != '') {
 			$stmt = $db->prepare("UPDATE produto SET design=:des WHERE ean=:ean");
-			$db->beginTransaction();
 			$stmt->execute(['des' => $newdesign, 'ean' => $ean]);
-			$db->commit();
-		}a
+		}
 
 	$db = null;
 }
