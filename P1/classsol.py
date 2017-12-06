c3ce75c21c34fbfa26e5ce63403e247a46244a1a
mihail <mihail.brinza96@hotmail.com>
2017-12-06 21:24:48 +0000

    perfect result reached

--- P1/classsol.py
+++ P1/classsol.py
@@ -11,22 +11,18 @@ def features(X):
     F = np.zeros((len(X), 5))
     for x in range(0, len(X)):
         F[x, 0] = len(X[x])
-        F[x, 1] = sum(letter in 'aeiouAEIOU' for letter in X)
-        F[x, 2] = sum(letter in 'aeiouAEIOU' for letter in X) % 2 == 0
-        F[x, 3] = (len(X[x]) - sum(letter in 'aeiouAEIOU' for letter in X)) % 2 == 0  # nr de consoantes par
-        F[x, 4] = X.count('a')
+        F[x, 1] = sum(letter in 'aeiouAEIOU' for letter in X[x])
+        F[x, 2] = sum(letter in 'aeiouAEIOU' for letter in X[x]) % 2 == 0
+        F[x, 3] = (len(X[x]) - sum(letter in 'aeiouAEIOU' for letter in X[x])) % 2 == 0  # nr de consoantes par
+        F[x, 4] = X[x].count('a')
     return F
 
 
 def mytraining(f, Y):
-    reg = neighbors.KNeighborsClassifier()
+    reg = neighbors.KNeighborsClassifier(3)
     reg = reg.fit(f, Y)
 
     return reg
-
-
-# def mytrainingaux(f, Y, par):
-# return reg
 
 
 def myprediction(f, reg):
