4aad2dac0487ab621ff6075016e9559273366db5
mihail <mihail.brinza96@hotmail.com>
2017-12-08 03:57:51 +0000

    progress made

--- P2/regsol.py
+++ P2/regsol.py
@@ -4,19 +4,22 @@ from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import cross_val_score
 import timeit
 
-def mytraining(X,Y):
-    
-   
-    return reg
-    
-def mytrainingaux(X,Y,par):
-    
-    reg.fit(X,Y)
-                
+
+def mytraining(X, Y):
+    reg = linear_model.ElasticNet()
+    reg = reg.fit(X, Y)
+
     return reg
 
-def myprediction(X,reg):
 
+def test_params(X, Y):
+    for i in range(1, 10, 2):
+        reg = KernelRidge(kernel="linear", alpha=i)
+        reg = reg.fit(X, Y)
+        print('kn' + str(i), -cross_val_score(reg, X, Y, cv=5, scoring='neg_mean_squared_error').mean())
+
+
+def myprediction(X, reg):
     Ypred = reg.predict(X)
 
     return Ypred
--- P2/testregsol.py
+++ P2/testregsol.py
@@ -9,24 +9,24 @@ from matplotlib.colors import ListedColormap
 
 import regsol
 
-tres = [.3, 800]    
-for ii,test in enumerate(["regress.npy", "regress2.npy"]):
+tres = [.3, 800]
+for ii, test in enumerate(["regress.npy", "regress2.npy"]):
     print("Testing " + test)
-    
-    X,Y,Xp,Yp = np.load(test)
-       
-    reg = regsol.mytraining(X,Y)
-    
-    Ypred = regsol.myprediction(Xp,reg)
-    
-    if -cross_val_score( reg, X, Y, cv = 5, scoring = 'neg_mean_squared_error').mean() < tres[ii]:
+    X, Y, Xp, Yp = np.load(test)
+    #regsol.test_params(X, Y)
+
+    reg = regsol.mytraining(X, Y)
+    print(tres[ii])
+
+    print(-cross_val_score(reg, X, Y, cv=5, scoring='neg_mean_squared_error').mean())
+    Ypred = regsol.myprediction(Xp, reg)
+
+    if -cross_val_score(reg, X, Y, cv=5, scoring='neg_mean_squared_error').mean() < tres[ii]:
         print("Erro dentro dos limites de tolerância. OK\n")
     else:
-        print("Erro acima dos limites de tolerância. FAILED\n")    
+        print("Erro acima dos limites de tolerância. FAILED\n")
     plt.figure()
-    plt.plot(Xp,Yp,'g.',label='datatesting')
-    plt.plot(X,Y,'k+',label='datatrain')
-    plt.plot(Xp,Ypred,'m',label='linregres1')
-    plt.legend( loc = 1 )
-
-
+    plt.plot(Xp, Yp, 'g.', label='datatesting')
+    plt.plot(X, Y, 'k+', label='datatrain')
+    plt.plot(Xp, Ypred, 'm', label='linregres1')
+    plt.legend(loc=1)
